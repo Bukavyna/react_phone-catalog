ee0ca97aa18a900a0ab4fa0aca6e47d5c4c3cf8d
Taras <luchakt2@gmail.com>
2026-01-24 14:28:09 +0200

    fix: replace BrowserRouter with HashRouter to fix white screen on GH Pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { App } from './App';
 
 import './styles/global.scss';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
-  <BrowserRouter
-    basename={import.meta.env.BASE_URL}
+  <HashRouter
+    // basename={import.meta.env.BASE_URL}
     future={{
       v7_startTransition: true,
       v7_relativeSplatPath: true,
     }}
   >
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
