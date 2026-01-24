7a51b5977403e22173e48fb629d46265865a190e
Taras <luchakt2@gmail.com>
2026-01-24 20:49:43 +0200

    ci: add github action for auto deploy

--- src/modules/Home/components/HeroBanner/HeroBanner.tsx
+++ src/modules/Home/components/HeroBanner/HeroBanner.tsx
@@ -8,9 +8,21 @@ import { ArrowButton } from '../../../../components/ArrowButton';
 
 export const HeroBanner: React.FC = () => {
   const banners = [
-    { id: 1, image: `${import.meta.env.BASE_URL}img/banner-phones.png`, link: 'phones' },
-    { id: 2, image: `${import.meta.env.BASE_URL}img/banner-tablets.png`, link: 'tablets' },
-    { id: 3, image: `${import.meta.env.BASE_URL}img/banner-accessories.png`, link: 'accessories' },
+    {
+      id: 1,
+      image: `${import.meta.env.BASE_URL}img/banner-phones.png`,
+      link: 'phones',
+    },
+    {
+      id: 2,
+      image: `${import.meta.env.BASE_URL}img/banner-tablets.png`,
+      link: 'tablets',
+    },
+    {
+      id: 3,
+      image: `${import.meta.env.BASE_URL}img/banner-accessories.png`,
+      link: 'accessories',
+    },
   ];
 
   const { currentIndex, next, prev, goTo } = useSlider({
@@ -35,11 +47,7 @@ export const HeroBanner: React.FC = () => {
           >
             {banners.map(banner => (
               <Link to={banner.link} key={banner.id} className={styles.slide}>
-                <img
-                  src={banner.image}
-                  alt="Banner"
-                  className={styles.image}
-                />
+                <img src={banner.image} alt="Banner" className={styles.image} />
               </Link>
             ))}
           </div>
